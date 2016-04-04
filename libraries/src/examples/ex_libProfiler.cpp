c13516c169b9300b131cc6593a651e6d6729ee41
jaejunlee <jaejun0201@gmail.com>
2016-04-04 12:12:20 +0900

    add thread to example

--- libraries/src/examples/ex_libProfiler.cpp
+++ libraries/src/examples/ex_libProfiler.cpp
@@ -25,8 +25,23 @@ void myfunction(){
     PROFILER_END();
 }
 
+pthread_t id1;
+void*testThread(void *pData){
+    id1 = pthread_self();
+    int count = 200;
+    while(count--){
+        PROFILER_START(testThread);
+        ::usleep(10000);
+        PROFILER_END();
+    }
+    return NULL;
+}
+
 int main()
 {
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, testThread, NULL);
+
     PROFILER_ENABLE;
     PROFILER_START(main);
     PROFILER_START(main_usleep);
@@ -35,7 +50,11 @@ int main()
     PROFILER_END();
     myfunction();
 
+//    std::cout<<thread_id<<","<<id1<<std::endl;
     PROFILER_END();
+    pthread_join(thread_id, NULL);
+
+    testThread(NULL);
     LogProfiler();
     PROFILER_DISABLE;
     return 0;
